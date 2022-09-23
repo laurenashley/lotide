af40082524bdeca20d546ff6ad10a4393829f423
Lauren Ashley <la.zaubi@gmail.com>
2022-09-22 22:54:52 -0700

    installed lodash and added fn to get first item of array

---
+++ head.js
@@ -0,0 +1,22 @@
+// Load full Lodash build
+var _ = require('lodash');
+
+// Implement Functions
+const assertEqual = function(actual, expected) {
+  const successMsg = `👍✅Assertion Passed: ${actual} === ${expected}`,
+    failMsg = `👎❌Assertion Failed: ${actual} === ${expected}`;
+
+  const result = (actual === expected) ? successMsg : failMsg;
+
+  console.log(result);
+};
+
+const head = function(array) {
+  return array.length > 1 ? _.head(array) : Number(array);
+;}
+
+//Test Code
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Lighthouse", "Labs"]), "Hello");
+assertEqual(head(7), 5);
