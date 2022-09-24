fddd0d1e46817943b94ade6f9b04d0c7fbbfbcef
Lauren Ashley <la.zaubi@gmail.com>
2022-09-23 21:56:26 -0700

    removed unnecessary code for a condition that would not happen - passing something other than array to head

--- head.js
+++ head.js
@@ -9,11 +9,12 @@ const assertEqual = function(actual, expected) {
 };
 
 const head = function(array) {
-  return array.length > 1 ? array[0] : Number(array);
+  return array[0];
 };
 
 //Test Code
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head(["Lighthouse", "Labs"]), "Hello");
-assertEqual(head(7), 5);
+assertEqual(head([7]), 5);
+assertEqual(head([57, 312, 0]), 57);
