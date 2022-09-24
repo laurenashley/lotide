2c169fe26af1a211825702cf346ff3940b5365c8
Lauren Ashley <la.zaubi@gmail.com>
2022-09-23 21:57:48 -0700

    added another test for failure

--- head.js
+++ head.js
@@ -18,3 +18,4 @@ assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head(["Lighthouse", "Labs"]), "Hello");
 assertEqual(head([7]), 5);
 assertEqual(head([57, 312, 0]), 57);
+assertEqual(head([57, 312, 0]), 312);
