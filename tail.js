65d4d0696fca4482e27c7a586557aaf8aeefaea4
Lauren Ashley <la.zaubi@gmail.com>
2022-09-24 17:23:50 -0700

    added tail fn and test cases to account for empty array or array with only one value.

--- tail.js
+++ tail.js
@@ -8,4 +8,25 @@ const assertEqual = function(actual, expected) {
   console.log(result);
 };
 
+const tail = function(array) {
+  let result = (array.length > 1) ? array.slice(1) : [];
+  return result;
+};
+
 // Test Code
+const words = ['hello', 'lighthouse', 'labs'];
+const result = tail(words);
+
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "labs"); // ensure second element is "Labs"
+
+const word = ['hello'];
+const result2 = tail(word);
+
+assertEqual(result2.length, 0); // ensure we get back zero elements
+
+const empty = [];
+const result3 = tail(empty);
+
+assertEqual(result3.length, 0); // ensure we get back zero elements
