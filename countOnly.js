f9f0835bb5c5301c4dd00102aa0ae2a13a0c7883
Lauren Ashley <la.zaubi@gmail.com>
2022-09-26 21:23:49 -0700

    added condition to only count items from itemsToCount

--- countOnly.js
+++ countOnly.js
@@ -14,10 +14,12 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-    if (results[item]) {
-      results[item] += 1;
-    } else {
-      results[item] = 1;
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
     }
   }
 
