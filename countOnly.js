0ed9fd35290c522ccca32741903816bdec7f4258
Lauren Ashley <la.zaubi@gmail.com>
2022-09-26 21:21:01 -0700

    added fn to count which items specified in one object exist in a given array

---
+++ countOnly.js
@@ -0,0 +1,44 @@
+// Function Implementation
+const assertEqual = function(actual, expected) {
+  const successMsg = `👍✅Assertion Passed: ${actual} === ${expected}`,
+    failMsg = `👎❌Assertion Failed: ${actual} === ${expected}`;
+
+  const result = (actual === expected) ? successMsg : failMsg;
+
+  console.log(result);
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+  }
+
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
