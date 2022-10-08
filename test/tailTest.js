5d4cf6407ff2c6cdb5a845f0d560787930d169a4
Lauren Ashley <la.zaubi@gmail.com>
2022-10-07 22:05:35 -0700

    refactored tailTest to use mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,22 +1,38 @@
 // test/tailTest.js
 
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
+
 
 // Test Code
 const words = ['hello', 'lighthouse', 'labs'];
 const result = tail(words);
 
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "labs"); // ensure second element is "Labs"
-
 const word = ['hello'];
 const result2 = tail(word);
-
-assertEqual(result2.length, 0); // ensure we get back zero elements
 
 const empty = [];
 const result3 = tail(empty);
 
-assertEqual(result3.length, 0); // ensure we get back zero elements
+describe("#head", () => {
+  it("returns ['lighthouse', 'labs'] for result", () => {
+    assert.deepEqual(result, ['lighthouse', 'labs']);
+  });
+  it("returns 2 for result.length", () => {
+    assert.strictEqual(result.length, 2);
+  });
+  it("returns 'lighthouse' for result[0]", () => {
+    assert.strictEqual(result[0], 'lighthouse');
+  });
+  it("returns 'labs' for result[1]", () => {
+    assert.strictEqual(result[1], 'labs');
+  });
+  it("returns 0 for result2.length", () => {
+    assert.strictEqual(result2.length, 0);
+  });
+  it("returns 0 for result3.length", () => {
+    assert.strictEqual(result3.length, 0);
+  });
+});
+
+// TO DO write more tests and use deepEqual
