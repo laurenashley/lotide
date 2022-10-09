c31ac06f94826ce49efe2aa3623e7a47f3fa473f
Lauren Ashley <la.zaubi@gmail.com>
2022-10-08 18:38:17 -0700

    shortened code using object shorthand

--- index.js
+++ index.js
@@ -4,8 +4,4 @@ const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
 
-module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle
-}
+module.exports = { head, tail, middle }
