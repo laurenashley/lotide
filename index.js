e75bedd05688d37bb7afcc7514027a819c0e8064
Lauren Ashley <la.zaubi@gmail.com>
2022-10-16 09:03:26 -0700

    resolved two testing errors, exported all objects used by lotide

--- index.js
+++ index.js
@@ -4,4 +4,21 @@ const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
 
-module.exports = { head, tail, middle }
+const flatten = require('./flatten');
+const countOnly = require('./countOnly');
+const letterPositions = require('./letterPositions');
+const findKeyByValue = require('./findKeyByValue');
+const eqObjects = require('./eqObjects');
+const eqArrays = require('./eqArrays');
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  flatten,
+  countOnly,
+  letterPositions,
+  findKeyByValue,
+  eqObjects,
+  eqArrays
+};
--- test/headTest.js
+++ test/headTest.js
@@ -13,14 +13,14 @@ describe("#head", () => {
   it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
     assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
   });
-  it("returns 'Hello' for ['Lighthouse', 'Labs']", () => {
-    assert.strictEqual(head(['Lighthouse', 'Labs']), 'Hello'); // TO DO
+  it("returns 'Lighthouse' for ['Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Lighthouse', 'Labs']), 'Lighthouse');
   });
   it("returns 57 for [57, 312, 0]", () => {
     assert.strictEqual(head([57, 312, 0]), 57);
   });
-  it("returns 312 for [57, 312, 0]", () => {
-    assert.strictEqual(head([57, 312, 0]), 312); // TO DO
+  it("returns 312 for [312, 57, 0]", () => {
+    assert.strictEqual(head([312, 57, 0]), 312);
   });
   it("returns 5 for [5,6,7]", () => {
     assert.strictEqual(head([5,6,7]), 5);
