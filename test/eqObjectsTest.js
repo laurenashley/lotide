cfbc86fe8d5dc8246e325ee7dadaa13faaabfc8f
Lauren Ashley <la.zaubi@gmail.com>
2023-05-04 13:39:01 -0700

    add tests for nested objects

--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -12,6 +12,60 @@ const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 const cd2 = { c: "1", d: ["2", 3, 4] };
 
+const nestedObjA = {
+  foo: 'bar'
+};
+const nestedObjB = {
+  foo: 'bar',
+  baz: { qux: 42 }
+};
+const nestedObjBa = {
+  baz: { qux: 42 },
+  foo: 'bar'
+};
+const nestedObjC = {
+  name: 'John Doe',
+  age: 35,
+  group: {
+    items: {
+      city: 'Anytown',
+      state: 'CA',
+      zip: '12345',
+      country: 'USA',
+      coordinates: {
+        latitude: 37.7749,
+        longitude: -122.4194,
+        evenmore: {
+          items: {
+            name: "hello"
+          }
+        }
+      }
+    }
+  }
+};
+const nestedObjCa = {
+  name: 'John Doe',
+  age: 35,
+  group: {
+    items: {
+      city: 'Anytown',
+      state: 'CA',
+      zip: '12345',
+      coordinates: {
+        latitude: 37.7749,
+        longitude: -122.4194,
+        evenmore: {
+          items: {
+            name: "hello"
+          }
+        }
+      }
+    }
+  }
+};
+
+
 describe("#eqObjects", () => {
   it("returns true for eqObjects(ab, ba)", () => {
     assert.strictEqual(eqObjects(ab, ba), true);
@@ -24,5 +78,14 @@ describe("#eqObjects", () => {
   });
   it("returns false for eqObjects(cd, cd2)", () => {
     assert.strictEqual(eqObjects(cd, cd2), false);
+  });
+  it('handles a single item object fine', () => {
+    assert.strictEqual(eqObjects(nestedObjA, nestedObjA), true);
+  });
+  it('handles one level of nesting fine', () => {
+    assert.strictEqual(eqObjects(nestedObjB, nestedObjBa), false);
+  });
+  it('handles multiple levels of nesting fine', () => {
+    assert.strictEqual(eqObjects(nestedObjC, nestedObjCa), false);
   });
 });
